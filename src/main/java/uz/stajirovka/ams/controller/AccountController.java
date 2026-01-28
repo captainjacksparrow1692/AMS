560159bd8cc4e20733dfd2d6af44053e5d981220
Jack Sparrow <captainjacksparrow12345piret@gmail.com>
2026-01-28 17:47:51 +0500

    update AccountController record

--- src/main/java/uz/stajirovka/ams/controller/AccountController.java
+++ src/main/java/uz/stajirovka/ams/controller/AccountController.java
@@ -3,6 +3,7 @@ package uz.stajirovka.ams.controller;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +16,8 @@ import uz.stajirovka.ams.dto.response.AccountCreateResponseDto;
 import uz.stajirovka.ams.dto.response.AccountInfoResponseDto;
 import uz.stajirovka.ams.dto.response.BalanceResponseDto;
 import uz.stajirovka.ams.service.AccountService;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/accounts")
@@ -47,5 +50,10 @@ public class AccountController {
     @PostMapping("/{accountNumber}/close")
     public AccountInfoResponseDto closeAccount(@PathVariable String accountNumber) {
         return accountService.closeAccount(accountNumber);
+    }
+
+    @GetMapping("/{id}")
+    public AccountInfoResponseDto getAccountById(@PathVariable UUID id) {
+        return accountService.getAccountById(id);
     }
 }
